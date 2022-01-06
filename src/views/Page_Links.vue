b7c8a8e10a1129261b09598300383afe55ff8e6c
iwasaki1005 <viszat@gmail.com>
2022-01-06 18:59:43 +0900

    goodsページを追加(各ショップへのリンクは随時追加予定)

---
+++ src/views/Page_Links.vue
@@ -0,0 +1,58 @@
+<template>
+	<section id="About" class="section">
+		<v-breadcrumbs :items="breadcrumbs" divider=">"></v-breadcrumbs>
+		<h2 class="font-E3 text-center"><span>Links</span></h2>
+		<div class="vertical-line center sa sa--up"></div>
+		<v-card theme="disable" class="bg-none sa sa--up">
+			<v-tabs v-model="tab" align-with-title grow>
+				<v-tabs-slider color="yellow"></v-tabs-slider>
+				<v-tab v-for="item in items" :key="item">
+					{{ item }}
+				</v-tab>
+			</v-tabs>
+			<v-tabs-items v-model="tab">
+				<v-tab-item>
+					<p>サイトとか</p>
+				</v-tab-item>
+				<v-tab-item>
+					<p>twitteeとか</p>
+				</v-tab-item>
+			</v-tabs-items>
+		</v-card>
+		<BackToHome />
+	</section>
+</template>
+
+<script>
+import BackToHome from '../components/BackToHome.vue'
+
+export default {
+	name: 'Page_Links',
+	components: {
+		BackToHome,
+	},
+	mounted: function() {
+		this.scrollanime_js()
+	},
+	data () {
+		return {
+			tab: null,
+			items: [
+			'さいと？', 'ゆーざー？',
+		],
+		breadcrumbs: [
+		{
+			text: 'Home',
+			disabled: false,
+			to: '/',
+		},
+		{
+			text: 'Links',
+			disabled: false,
+			href: '',
+		},
+		],
+		}
+	},
+}
+</script>
