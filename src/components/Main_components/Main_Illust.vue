fb535b7adfce96b354622a48660e5f6453619f1c
iwasaki1005 <viszat@gmail.com>
2022-01-18 17:33:56 +0900

    mainページのgallery機能を追加

--- src/components/Main_components/Main_Illust.vue
+++ src/components/Main_components/Main_Illust.vue
@@ -1,12 +1,15 @@
 <template>
-	<v-lazy v-model="isActive" :options="{ threshold: 1}" min-height="100" transition="fade-transition">
+	<div>
+		<transition name="vfade">
+			<GalleryGallerymodal  v-show="modalscreen" :list="list" :difil="difil" @illustselect="illustselect" @differenceselect="differenceselect" @screenclose="screenclose" :seldes="seldes" :sellinktxt="sellinktxt" :sellinksrc="sellinksrc" @modalfuncopen="modalfuncopen" @modalfuncclose="modalfuncclose"/>
+		</transition>
 		<section id="Illust" class="section-2 sa">
 			<h2 class="font-E3 text-center scroll-fadein index-anime"><span>three</span></h2>
 			<div class="vertical-line center sa sa--up"></div>
 			<div class="display-flex">
 				<div class="width95">
 					<div class="space-around sa sa--up">
-						<div class="il-1 flex-lower-right hover-flash">
+						<div class="il-1 flex-lower-right hover-flash" @click="screenopen(1)" v-bind:class="list[1].thumclass" v-show="list[1].active" v-bind:data-sa_delay="index * 1000">
 							<img class="bt-il-open m-bottom-05rem m-right-05rem" src="../../assets/img/bt-open.svg">
 						</div>
 						<div class="space-around-column">
@@ -42,14 +45,86 @@
 				</a>
 			</div>
 		</section>
-	</v-lazy>
+	</div>
 </template>
 
 <script>
+import GalleryGallerymodal from '../Gallery_components/Gallery_gallerymodal.vue'
+
 export default {
   name: 'MainIllust',
+  components: {
+		GalleryGallerymodal,
+  },
   data: () => ({
       isActive: false,
+      list : [
+			{
+				title:"イラスト1 男の子 女の子 複数人", 
+				thumclass:["bgil-1"], differenceses:[], 
+				src: require('../../uploads/illustrations/イラスト1、男の子、女の子、複数人.png'),
+				difsrc:[],
+				tag:["男の子", "女の子","複数人", "rating=a"], active: true,
+				description:'ナントカさんで描かせていただいたタペストリーのイラストです。CUFF様にて通販委託中です。',
+				linktext: 'CUFF様通販SHOP', linksrc: 'https://9-nine-project.com/special/',
+			},
+			{
+				title:"イラスト2 男の子 ヤンキー 複数人", 
+				thumclass:["bgil-2"], differenceses:[], 
+				src: require('../../uploads/illustrations/イラスト2、男の子、複数人、ヤンキー.png'),
+				difsrc:[],
+				tag:["男の子", "ヤンキー","複数人", "rating=a"], active: true,
+				description:'',
+				linktext: '', linksrc: '',
+			},
+			{
+				title:"イラスト3 女の子", 
+				thumclass:["bgil-3"], differenceses:["bgil-3-2"], 
+				src: require('../../uploads/illustrations/イラスト3、女の子.png'),
+				difsrc:[require('../../uploads/illustrations/イラスト3-2、女の子.png')],
+				tag:["女の子", "rating=a"], active: true,
+				description:'商品説明みたいな感じ',
+				linktext: 'Amazon', linksrc: 'https://9-nine-project.com/special/',
+			},
+			{
+				title:"イラスト4 女の子 !特殊", 
+				thumclass:["bgil-4"], differenceses:[], 
+				src: require('../../uploads/illustrations/イラスト4、女の子、特殊.png'),
+				difsrc:[],
+				tag:["女の子", "rating=r18"], active: true,
+				description:'',
+				linktext: '', linksrc: '',
+			},
+			{
+				title:"イラスト5 女の子", 
+				thumclass:["bgil-5"], differenceses:["bgil-5-2","bgil-5-3"], 
+				src: require('../../uploads/illustrations/イラスト5、女の子、男の子.png'),
+				difsrc:[require('../../uploads/illustrations/イラスト5-2、女の子、男の子.png'),
+						require('../../uploads/illustrations/イラスト5-3、女の子、男の子.png')],
+				tag:["女の子", "rating=a"], active: true,
+				description:'',
+				linktext: '', linksrc: '',
+			},
+			{
+				title:"イラスト6 女の子 複数人", 
+				thumclass:["bgil-6"], differenceses:[], 
+				src: require('../../uploads/illustrations/イラスト６、女の子、複数人.png'),
+				difsrc:[],
+				tag:["女の子","複数人", "rating=a"], active: true,
+				description:'',
+				linktext: '', linksrc: '',
+			},
+		],
+		difil: [],
+		difilsrc: [],
+		seldes: '',
+		sellinktxt: '',
+		sellinksrc: '',
+		rating: false,
+		activetag: [],
+		alltags: ["男の子","女の子","複数人","ヤンキー",],
+		modalscreen: false,
+		overflowyhide: false,
   }),
   updated: function () {
 		this.indexanime()
