ca1ddaeb35011d5299677bae0bd49e8c2e2287b1
iwasaki1005 <viszat@gmail.com>
2022-01-18 13:38:08 +0900

    ダミーパーツを削除

--- src/views/Page_Main.vue
+++ src/views/Page_Main.vue
@@ -4,24 +4,7 @@
 		<MainNews />
 		<MainGame />
 		<MainIllust />
-		<!--
-		<div id="Top-il" class="tp-il-bg column-center">
-			<img class="tp-il-img" src="https://3.bp.blogspot.com/-joXYE8YRH-w/VwIjIGpt9-I/AAAAAAAA5fM/jHtT9Wiwdtgas3DI6gSgxnb19CLZ4_gAw/w1200-h630-p-k-no-nu/internet_offline_offkai.png">
-			<div class="tp-il-text m-top-1rem p-tb-05rem p-rl-2rem">
-				<div class="space-around">
-				</div>
-				<p class="co-white">
-					説明文
-				</p>
-				<p class="co-white">
-					関連リンク
-				</p>
-			</div>
-			<img class="bt-il-close m-top-1rem" src="../assets/img/bt-close.svg">
-		</div>
-		-->
 		<MainProfile />
-		<div class="dmy"></div>
 	</div>
 </template>
 
