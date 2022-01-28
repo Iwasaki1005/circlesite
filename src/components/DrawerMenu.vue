029f067dcad588471700a30e4943782d0aa19a2d
iwasaki1005 <viszat@gmail.com>
2022-01-28 14:54:01 +0900

    ドロワーメニューのデザインを再調整

--- src/components/DrawerMenu.vue
+++ src/components/DrawerMenu.vue
@@ -7,29 +7,29 @@
 			<div class="dr-links">
 				<div id="dr-content1" class="dr-container dr-saup opacity-0">
 					<router-link to="/">
-						<div class="display-flex dr-link closeevent">
-							<i class="fas fa-home co-brown dr-icon m-right-05rem"></i>
+						<div class="display-flex dr-link dr-link-w50_mobile closeevent">
+							<i class="fas fa-home co-brown co-brown_mobile dr-icon m-right-05rem co-brown"></i>
 							<div>
-								<p class="font-E2 dr-fs1"><span class="co-brown">H</span><span class="co-grey">ome</span></p>
-								<p class="dr-fs2 co-grey">ホーム</p>
+								<p class="font-E2 dr-fs1"><span class="co-brown co-brown_mobile">H</span><span class="co-grey co-grey_mobile">ome</span></p>
+								<p class="dr-fs2 co-grey co-grey_mobile">ホーム</p>
 							</div>
 						</div>
 					</router-link>
 					<router-link to="/about">
 						<div class="display-flex dr-link closeevent">
-							<i class="fas fa-object-group co-brown dr-icon m-right-05rem"></i>
+							<i class="fas fa-object-group co-brown co-brown_mobile dr-icon m-right-05rem"></i>
 							<div>
-								<p class="font-E2 dr-fs1"><span class="co-brown">A</span><span class="co-grey">bout</span></p>
-								<p class="dr-fs2 co-grey">概要</p>
+								<p class="font-E2 dr-fs1"><span class="co-brown co-brown_mobile">A</span><span class="co-grey co-grey_mobile">bout</span></p>
+								<p class="dr-fs2 co-grey co-grey_mobile">概要</p>
 							</div>
 						</div>
 					</router-link>
 					<router-link to="/news">
 						<div class="display-flex dr-link closeevent">
-							<i class="fas fa-newspaper co-brown dr-icon m-right-05rem"></i>
+							<i class="fas fa-newspaper co-brown co-brown_mobile dr-icon m-right-05rem"></i>
 							<div>
-								<p class="font-E2 dr-fs1"><span class="co-brown">N</span><span class="co-grey">ews</span></p>
-								<p class="dr-fs2 co-grey">最新情報</p>
+								<p class="font-E2 dr-fs1"><span class="co-brown co-brown_mobile">N</span><span class="co-grey co-grey_mobile">ews</span></p>
+								<p class="dr-fs2 co-grey co-grey_mobile">最新情報</p>
 							</div>
 						</div>
 					</router-link>
@@ -37,37 +37,37 @@
 				<div id="dr-content2" class="dr-container dr-saup opacity-0">
 					<router-link to="/gallery">
 						<div class="display-flex dr-link closeevent">
-							<i class="fas fa-pen-alt co-brown dr-icon m-right-05rem"></i>
+							<i class="fas fa-pen-alt co-brown co-brown_mobile dr-icon m-right-05rem"></i>
 							<div>
-								<p class="font-E2 dr-fs1"><span class="co-brown">G</span><span class="co-grey">allery</span></p>
-								<p class="dr-fs2 co-grey">イラスト一覧</p>
+								<p class="font-E2 dr-fs1"><span class="co-brown co-brown_mobile">G</span><span class="co-grey co-grey_mobile">allery</span></p>
+								<p class="dr-fs2 co-grey co-grey_mobile">イラスト一覧</p>
 							</div>
 						</div>
 					</router-link>
 					<router-link to="/game">
 						<div class="display-flex dr-link closeevent">
-							<i class="fas fa-gamepad co-brown dr-icon m-right-05rem"></i>
+							<i class="fas fa-gamepad co-brown co-brown_mobile dr-icon m-right-05rem"></i>
 							<div>
-								<p class="font-E2 dr-fs1"><span class="co-brown">G</span><span class="co-grey">ame</span></p>
-								<p class="dr-fs2 co-grey">ゲーム</p>
+								<p class="font-E2 dr-fs1"><span class="co-brown co-brown_mobile">G</span><span class="co-grey co-grey_mobile">ame</span></p>
+								<p class="dr-fs2 co-grey co-grey_mobile">ゲーム</p>
 							</div>
 						</div>
 					</router-link>
 					<router-link to="/shop">
 						<div class="display-flex dr-link closeevent">
-							<i class="fas fa-store-alt co-brown dr-icon m-right-05rem"></i>
+							<i class="fas fa-store-alt co-brown co-brown_mobile dr-icon m-right-05rem"></i>
 							<div>
-								<p class="font-E2 dr-fs1"><span class="co-brown">S</span><span class="co-grey">hop</span></p>
-								<p class="dr-fs2 co-grey">ショップ</p>
+								<p class="font-E2 dr-fs1"><span class="co-brown co-brown_mobile">S</span><span class="co-grey co-grey_mobile">hop</span></p>
+								<p class="dr-fs2 co-grey co-grey_mobile">ショップ</p>
 							</div>
 						</div>
 					</router-link>
 					<router-link to="/works">
 						<div class="display-flex dr-link closeevent">
-							<i class="fas fa-tablet-alt co-brown dr-icon m-right-05rem"></i>
+							<i class="fas fa-tablet-alt co-brown co-brown_mobile dr-icon m-right-05rem"></i>
 							<div>
-								<p class="font-E2 dr-fs1"><span class="co-brown">W</span><span class="co-grey">orks</span></p>
-								<p class="dr-fs2 co-grey">活動・お仕事履歴</p>
+								<p class="font-E2 dr-fs1"><span class="co-brown co-brown_mobile">W</span><span class="co-grey co-grey_mobile">orks</span></p>
+								<p class="dr-fs2 co-grey co-grey_mobile">活動・お仕事履歴</p>
 							</div>
 						</div>
 					</router-link>
@@ -76,28 +76,28 @@
 				<div id="dr-content4" class="dr-container dr-saup opacity-0">
 					<router-link to="/information">
 						<div class="display-flex dr-link closeevent">
-							<i class="fas fa-file-alt co-brown dr-icon m-right-05rem"></i>
+							<i class="fas fa-file-alt co-brown co-brown_mobile dr-icon m-right-05rem"></i>
 							<div>
-								<p class="font-E2 dr-fs1"><span class="co-brown">I</span><span class="co-grey">nfo</span></p>
-								<p class="dr-fs2 co-grey">ご案内など</p>
+								<p class="font-E2 dr-fs1"><span class="co-brown co-brown_mobile">I</span><span class="co-grey co-grey_mobile">nfo</span></p>
+								<p class="dr-fs2 co-grey co-grey_mobile">ご案内など</p>
 							</div>
 						</div>
 					</router-link>
 					<router-link to="/links">
 						<div class="display-flex dr-link closeevent">
-							<i class="fas fa-external-link-alt co-brown dr-icon m-right-05rem"></i>
+							<i class="fas fa-external-link-alt co-brown co-brown_mobile dr-icon m-right-05rem"></i>
 							<div>
-								<p class="font-E2 dr-fs1"><span class="co-brown">L</span><span class="co-grey">ink</span></p>
-								<p class="dr-fs2 co-grey">リンク</p>
+								<p class="font-E2 dr-fs1"><span class="co-brown co-brown_mobile">L</span><span class="co-grey co-grey_mobile">ink</span></p>
+								<p class="dr-fs2 co-grey co-grey_mobile">リンク</p>
 							</div>
 						</div>
 					</router-link>
 					<router-link to="/contact">
 						<div class="display-flex dr-link closeevent">
-							<i class="fas fa-envelope co-brown dr-icon m-right-05rem"></i>
+							<i class="fas fa-envelope co-brown co-brown_mobile dr-icon m-right-05rem"></i>
 							<div>
-								<p class="font-E2 dr-fs1"><span class="co-brown">C</span><span class="co-grey">ontact</span></p>
-								<p class="dr-fs2 co-grey">連絡・お問い合わせ</p>
+								<p class="font-E2 dr-fs1"><span class="co-brown co-brown_mobile">C</span><span class="co-grey co-grey_mobile">ontact</span></p>
+								<p class="dr-fs2 co-grey co-grey_mobile">連絡・お問い合わせ</p>
 							</div>
 							</div>
 				</router-link>
