045844fc0972f437793dbee90a10fe9763c1aecb
iwasaki1005 <viszat@gmail.com>
2022-01-31 14:41:33 +0900

    backtohomeのモバイル用のデザインを作成

--- src/components/BackToHome.vue
+++ src/components/BackToHome.vue
@@ -1,6 +1,6 @@
 <template>
 	<router-link to="/">
-		<div class="content-core m-top-2rem sa sa--up">
+		<div class="content-core content-core_mobile m-top-2rem m-top-2rem_mobile sa sa--up">
 			<div class="bt-typeA bt-typeA-border d-block center">
 				<span class="bt-typeA-border-inner font-E1">Back To Home ></span>
 			</div>
