5b417abb8d918f74aeb3a8cdac760a12499013cd
iwasaki1005 <viszat@gmail.com>
2022-02-02 18:26:07 +0900

    インフォメーションのモバイル用のデザインを作成

--- src/views/Page_Information.vue
+++ src/views/Page_Information.vue
@@ -13,7 +13,7 @@
 			<v-tabs-items v-model="tab">
 				<v-tab-item>
 					<div class="NW-container">
-						<p class="info-indexbg m-bottom-1rem co-white">プライバシーポリシー</p>
+						<p class="info-indexbg m-bottom-1rem m-bottom-05rem_mobile co-white co-white_mobile">プライバシーポリシー</p>
 						<p>
 							※googleのやつをコピペ
 						</p>
@@ -21,33 +21,33 @@
 				</v-tab-item>
 				<v-tab-item>
 					<div class="NW-container">
-						<p class="info-indexbg m-bottom-1rem co-white">当サイトへのリンクについて</p>
+						<p class="info-indexbg m-bottom-1rem m-bottom-05rem_mobile co-white co-white_mobile">当サイトへのリンクについて</p>
 						<p>
 							当サイトは基本的にリンクフリーです。<br>
 							リンクをしていただく際のURLはトップページへお願いします。<br><br>
 							サイトへリンクされる際にバナーをご使用される場合につきましてはこちらで用意したバナーをお使いください。<br>
 						</p>
-						<p class="info-indexbg m-bottom-1rem co-white m-top-2rem">バナー</p>
+						<p class="info-indexbg m-bottom-1rem m-bottom-05rem_mobile co-white co-white_mobile m-top-2rem m-top-2rem_mobile">バナー</p>
 						<p class="">
 							バナーの画像を直リンクでご利用される場合はバナー下のコードをコピーして貼り付けることでご利用いただけます
 						</p>
-						<p class="fs-0_8rem co-grey">
+						<p class="fs-0_8rem fs-0_8rem_mobile co-grey co-grey_mobile">
 							※直リンクでご利用される場合、URLの変更やサイトのメンテナンス等で利用できなくなる場合がありますのでご注意ください。
 						</p>
-						<img class="m-top-1rem m-bottom-05rem" src="../assets/banner/neverlandf_banner1.png">
-						<textarea readonly onclick="this.select();" class="width100 df-textarea">
+						<img class="m-top-1rem m-top-1rem_mobile m-bottom-05rem m-bottom-05rem_mobile" src="../assets/banner/neverlandf_banner1.png">
+						<textarea readonly onclick="this.select();" class="width100 width100_mobile df-textarea">
 							"<a target="_blank" href="https://circus-co.jp/product/dc4/"><img alt="この恋は世界を変える CIRCUS 20th Project「D.C.4 ～ダ・カーポ4～」好評発売中!" title="この恋は世界を変える CIRCUS 20th Project「D.C.4 ～ダ・カーポ4～」好評発売中!" src="https://circus-co.jp/product/dc4/rc/special/dc4_banner_650x120.jpg"></a>"
 						</textarea>
 					</div>
 				</v-tab-item>
 				<v-tab-item>
 					<div class="NW-container">
-						<p class="info-indexbg m-bottom-1rem co-white">閲覧推奨ブラウザ</p>
-						<p class="m-bottom-1rem">当サイトを閲覧する際の推奨ブラウザは下記のものになります。</p>
-						<p class="m-bottom-1rem">
+						<p class="info-indexbg m-bottom-1rem m-bottom-05rem_mobile co-white co-white_mobile">閲覧推奨ブラウザ</p>
+						<p class="m-bottom-1rem m-bottom-05rem_mobile">当サイトを閲覧する際の推奨ブラウザは下記のものになります。</p>
+						<p class="m-bottom-1rem m-bottom-05rem_mobile">
 							・Google Chrome
 						</p>
-						<p class="fs-0_8rem co-grey">
+						<p class="fs-0_8rem fs-0_8rem_mobile co-grey co-grey_mobile">
 							Google Chromeは、Google Inc.の米国およびその他の国における商標または登録商標です。
 						</p>
 					</div>
