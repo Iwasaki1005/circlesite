5760270f0bac2fe5a1be814ddc3a373becc12d19
iwasaki1005 <viszat@gmail.com>
2022-04-08 13:53:48 +0900

    メイントップ部分のリファクタリング

--- src/components/Main_components/Main_Top.vue
+++ src/components/Main_components/Main_Top.vue
@@ -1,25 +1,24 @@
 <template>
-	<div id="NeverLand-top" class="nla-top">
-		<div class="nla-dia1-con">
-			<img class="nla-dia1" src="../../assets/sprite/triangle1.svg">
+	<div id="NeverLand-top" class="main-top">
+		<div class="main-top-dia-con">
+			<img class="main-top-dia" src="../../assets/sprite/triangle1.svg">
 		</div>
-		<!-- <div class="nla-dia2-con">
-			<img class="nla-dia1" src="../../assets/sprite/triangle2.svg">
-		</div> -->
-		<div class="nla-logo-grid content-core content-core_mobile">
-			<img class="nla-logo" src="../../assets/logo/neverlandf_logo1.png" alt=”ねばーランドFロゴ”>
+		<div class="main-top-logo-grid f-ali-jus-center">
+			<img class="main-top-logo" src="../../assets/logo/neverlandf_logo1.png" alt=”ねばーランドFロゴ”>
 		</div>
-		<div class="nla-tips1">
-			<img class="nla-banner tr-duration-025s" src="../../assets/dmyimage/topicbanner_dmy1.png">
-			<img class="nla-banner tr-duration-025s" src="../../assets/dmyimage/topicbanner_dmy2.png">
+		<div class="main-top-tips1">
+			<img class="main-top-banner tr-duration-025s" src="../../assets/dmyimage/topicbanner_dmy1.png">
+			<img class="main-top-banner tr-duration-025s" src="../../assets/dmyimage/topicbanner_dmy2.png">
 		</div>
-		<div class="nla-tips2">
-			<img class="nla-banner tr-duration-025s" src="../../assets/dmyimage/topicbanner_dmy3.png">
-			<img class="nla-banner tr-duration-025s" src="../../assets/dmyimage/topicbanner_dmy1.png">
-			<img class="nla-banner tr-duration-025s" src="../../assets/dmyimage/topicbanner_dmy2.png">
-			<img class="nla-banner tr-duration-025s" src="../../assets/dmyimage/topicbanner_dmy3.png">
+		<div class="main-top-tips2">
+			<img class="main-top-banner tr-duration-025s" src="../../assets/dmyimage/topicbanner_dmy3.png">
+			<img class="main-top-banner tr-duration-025s" src="../../assets/dmyimage/topicbanner_dmy1.png">
+			<img class="main-top-banner tr-duration-025s" src="../../assets/dmyimage/topicbanner_dmy2.png">
+			<img class="main-top-banner tr-duration-025s" src="../../assets/dmyimage/topicbanner_dmy3.png">
 		</div>
-		<div style="display: none;" class="nla-visual">visual</div>
+		<div class="main-top-visual">
+			<!-- visual -->
+		</div>
 	</div>
 </template>
 
@@ -31,3 +30,133 @@ export default {
   }),
 }
 </script>
+
+<style>
+@media screen and (min-aspect-ratio: 1/1) {
+	.main-top {
+		height: 140vh;
+		min-height: 60vw;
+		width: 100%;
+		display: grid;
+		grid-template-rows: minmax(34vw, 80vh) minmax(8vw, 20vh) minmax(17vw, 40vh);
+		grid-template-columns: 30% 20% 50%;
+	}
+
+	.main-top-logo {
+		width: 25vw;
+	}
+
+	.main-top-logo-grid {
+		grid-row: 1 / 2;
+		grid-column: 1 / 3;
+	}
+
+	.main-top-tips1 {
+		grid-row: 2 / 3;
+		grid-column: 1 / 3;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+		padding: 0 1.5rem;
+	}
+
+	.main-top-tips2 {
+		grid-row: 3 / 4;
+		grid-column: 1 / 3;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+		flex-wrap: wrap;
+		padding: 0 1.5rem;
+	}
+
+	.main-top-visual {
+		grid-row: 1 / 4;
+		grid-column: 2 / 4;
+		/* background-color: rgba(252, 154, 129, 0.479); */
+	}
+
+	.main-top-banner {
+		width: 20vw;
+		box-shadow: 0 0 0.8vw 0.1vw #e7b06c;
+	}
+
+	.main-top-banner:hover {
+		width: 20vw;
+		box-shadow: 0 0 0.8vw 0.1vw #f69ac3;
+	}
+
+	.main-top-dia-con {
+		grid-row: 1 / 3;
+		grid-column: 1 / 2;
+		display: flex;
+		align-items: flex-start;
+	}
+
+	.main-top-dia {
+		width: 10vw;
+	}
+}
+
+@media screen and (max-aspect-ratio: 1/1) {
+	.main-top {
+		height: 137.5vh;
+		width: 100%;
+		display: grid;
+		grid-template-rows: 100vh 12.5vh 25vh;
+		grid-template-columns: 20% 30% 50%;
+	}
+
+	.main-top-logo {
+		width: 60vw;
+		margin-bottom: 1rem;
+		margin-left: 0.5rem;
+	}
+
+	.main-top-logo-grid {
+		grid-row: 1 / 2;
+		grid-column: 1 / 4;
+		display: flex;
+		align-items: flex-end;
+		justify-content: flex-start;
+	}
+
+	.main-top-tips1 {
+		grid-row: 2 / 3;
+		grid-column: 1 / 4;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+	}
+
+	.main-top-tips2 {
+		grid-row: 3 / 4;
+		grid-column: 1 / 4;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+		flex-wrap: wrap;
+	}
+
+	.main-top-visual {
+		grid-row: 1 / 3;
+		grid-column: 1 / 4;
+	}
+
+	.main-top-banner {
+		width: 40vw;
+		box-shadow: 0 0 0.8vw 0.1vw #e7b06c;
+	}
+
+	.main-top-dia {
+		width: 30vw;
+	}
+
+	.main-top-dia-con {
+		grid-row: 1 / 2;
+		grid-column: 1 / 3;
+		display: flex;
+		align-items: flex-start;
+	}
+}
+</style>
