1240439b6f617ff53c4421a0bcd07bed6596e958
iwasaki1005 <viszat@gmail.com>
2022-04-15 19:48:32 +0900

    vue headの導入

--- src/App.vue
+++ src/App.vue
@@ -69,6 +69,26 @@
 			'$route' (routeInstance,) {
 				this.createTitleDesc(routeInstance);
 			}
-		}
+		},
+		head: {
+			meta: [
+				{ name: 'keywords', content: 'ねばーランドF,イラスト,フリーゲーム' },
+				{ name: 'copyright', content: '© NEVERLAND.F' },
+				{ name: 'author', content: 'ねばーランド.F' },
+				{ property: 'og:title', content: 'ねばーランド.F' },
+				{ property: 'og:description', content: 'サークル「ネバーランドF」のオフィシャルウェブページです。製作したイラスト、ゲームなどを公開しています。' },
+				{ property: 'og:type', content: 'website' },
+				{ property: 'og:url', content: 'http://neverland-f.com/' },
+				{ property: 'og:locale', content: 'ja_JP' },
+				// { property: 'og:image', content: 'https://example.com/img/og.png' },
+				// { name: 'twitter:card', content: 'summary_large_image' },
+				// { name: 'twitter:image', content: 'https://example.com/img/og.png' }
+			],
+			link: [
+				{ rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css' },
+				{ rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css' },
+				// { rel: 'icon', href: require('./path/to/icon-16.png'), sizes: '16x16', type: 'image/png' }, 
+			],
+		},
 	}
 </script>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import vuetify from '@/plugins/vuetify' // path to vuetify export
 import router from './router'
 import VModal from 'vue-js-modal'
 import VueSocialSharing from 'vue-social-sharing'
+import VueHead from 'vue-head'
 import Globalfunctions from '../public/js/Globalfunctions.js'
 import Galleryfunctions from '../public/js/Galleryfunctions.js'
 import '../public/css/master.css'
@@ -29,6 +30,7 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 Vue.config.productionTip = false
 Vue.use(VModal);
 Vue.use(VueSocialSharing);
+Vue.use(VueHead)
 Vue.mixin(Globalfunctions)
 Vue.mixin(Galleryfunctions)
 
