3e29997cd3ff1a364bee955d90d40a5d84716a6c
iwasaki1005 <viszat@gmail.com>
2022-04-22 16:38:08 +0900

    やっぱりアナリティクスをhtmlに戻す

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import router from './router'
 import VModal from 'vue-js-modal'
 import VueSocialSharing from 'vue-social-sharing'
 import VueHead from 'vue-head'
-import VueAnalytics from 'vue-analytics';
 import Globalfunctions from '../public/js/Globalfunctions.js'
 import Galleryfunctions from '../public/js/Galleryfunctions.js'
 import '../public/css/master.css'
@@ -33,10 +32,6 @@ Vue.use(VueSocialSharing);
 Vue.use(VueHead)
 Vue.mixin(Globalfunctions)
 Vue.mixin(Galleryfunctions)
-Vue.use(VueAnalytics, {
-	id: 'G-15Y7NXCQG1',
-	router
-});
 
 new Vue({
   vuetify,
